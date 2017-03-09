273b3d69cbdb95c42ce6b274d5cd2e6efbe78a00
miterran <ejade86@hotmail.com>
2017-03-08 23:32:10 -0800

    Update auth.js

--- Desktop/google_place_api/config/auth.js
+++ Desktop/google_place_api/config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : '433321327059622',
 		'clientSecret' : '369f8146e87bfe3afb274091177247e0',
-		'callbackURL' : 'http://localhost:8080/facebook/callback'
+		'callbackURL' : 'https://protected-brook-14915.herokuapp.com/facebook/callback'
 	}
 }
 
